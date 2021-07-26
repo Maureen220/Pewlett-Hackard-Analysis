28ad1fb153a2e6429df3db2f28c4a2378c42f608
[Maureen Bayne] <[maureenbayne34@gmail.com]>
2021-07-26 13:55:31 -0600

    added current_emp.csv file

--- queries.sql
+++ queries.sql
@@ -179,9 +179,37 @@ SELECT  dm.dept_no,
         ce.first_name,
         dm.from_date,
         dm.to_date
--- INTO manager_info
+INTO manager_info
 FROM dept_manager AS dm
     INNER JOIN departments AS d
         ON (dm.dept_no = d.dept_no)
     INNER JOIN current_emp AS ce
         ON (dm.emp_no = ce.emp_no);
+
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--Create a query that will return only the information relevant to the Sales team
+SELECT di.emp_no,
+    di.first_name,
+	di.last_name,
+    di.dept_name
+FROM dept_info as di
+WHERE di.dept_name = 'Sales';
+
+--Create query that will return information for the Sales and Development teams
+SELECT di.emp_no,
+    di.first_name,
+	di.last_name,
+    di.dept_name
+FROM dept_info as di
+WHERE di.dept_name IN ('Sales', 'Development');
